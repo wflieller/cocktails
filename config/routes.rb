431bfd02891ae54d7a4ac9345ffa052938fb9d49
alwan mortada <almortada88@gmail.com>
2015-06-23 15:30:23 -0700

    added signup and log_in with sorcory

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ root "drinks#index"
 
 resources :sessions, only: [:new, :create, :destroy]
 get '/log_in', to: 'sessions#new', as: :log_in
-delete '/log_out', to: 'sessions#destroy', as: :log_out
+get '/log_out', to: 'sessions#destroy', as: :log_out
 
 
 resources :users, only: [:new, :create]
